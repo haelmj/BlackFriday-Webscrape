ac0d1613b3e6935ea446c146013e2cd491a3977d
Michael Ndon <manj19.mjn@gmail.com>
2020-11-03 17:25:44 +0100

    Corrected Typo in import statement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ subject-website: jumia.com
 """
 
 from selenium import webdriver
-from selenium.webdriver.coomon.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
