8c1d2962416754f87c7417e659031b334c7bd2f3
Michael Ndon <manj19.mjn@gmail.com>
2020-11-03 17:25:01 +0100

    Imported modules; initialized chrome driver and loaded webpage

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ title: Blackfridayscrape
 subject-website: jumia.com
 
 """
+
+from selenium import webdriver
+from selenium.webdriver.coomon.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH = "C:/Program Files (x86)/chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://www.jumia.com.ng/mlp-black-friday/?rating=4-5')
+black_deals_window = driver.window_handles[0]
+FOLDER = './categories'
+
