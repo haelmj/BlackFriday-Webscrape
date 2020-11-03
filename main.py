759b4554dbb6417b84b2b71181e5921dab4bf8c9
Michael Ndon <manj19.mjn@gmail.com>
2020-11-03 17:26:48 +0100

    Added more imports

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+import csv
+import time
 
 PATH = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -17,4 +20,7 @@ driver = webdriver.Chrome(PATH)
 driver.get('https://www.jumia.com.ng/mlp-black-friday/?rating=4-5')
 black_deals_window = driver.window_handles[0]
 FOLDER = './categories'
+
+
+
 
