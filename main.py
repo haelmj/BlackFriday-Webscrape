e790d4be7d99638ed72b1835be335cbe03489da1
Michael Ndon <manj19.mjn@gmail.com>
2020-11-04 22:58:07 +0100

    Added functionality to create folders for different categories

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import csv
 import time
+import os
 
 # set path to chromedriver and initialize chrome webdriver
-STARTPAGE = 'https://www.jumia.com.ng/mlp-black-friday/?rating=4-5'
+STARTPAGE = 'https://www.jumia.com.ng/mlp-black-friday/?sort=lowest-price&rating=4-5'
 PATH = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 # load web page; set window handle to 0; set storage folder
 driver.get(STARTPAGE)
 black_deals_window = driver.window_handles[0]
-FOLDER = './categories'
+FOLDER = 'categories'
 
 # the fun begins here...
+def create_folder(name):
+    os.makedirs(os.path.join(FOLDER, name))
+
 # get Cateogry names
 def category_name():
     # look for article tag
@@ -35,6 +39,7 @@ def category_name():
     title = category.find_element_by_tag_name('h2').get_attribute('textContent')
     elements = category.find_elements_by_tag_name('a')
     categories = [a.get_attribute('textContent') for a in elements]
+    for i in categories: create_folder(i)
     return categories
 
 print(category_name())
