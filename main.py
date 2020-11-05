74c2ca5caf886f54a1542c6240f0e453e6b61817
Michael Ndon <manj19.mjn@gmail.com>
2020-11-05 01:18:05 +0100

    Added function to get sub categories

--- main.py
+++ main.py
@@ -30,16 +30,29 @@ def create_folder(name):
     os.makedirs(os.path.join(FOLDER, name))
 
 # get Cateogry names
-def category_name():
+def get_category_names():
     # look for article tag
     category = WebDriverWait(driver, 100).until(
         EC.presence_of_element_located((By.TAG_NAME, 'article'))
         )
     # get category title and textcontent of a tags
-    title = category.find_element_by_tag_name('h2').get_attribute('textContent')
+    # title = category.find_element_by_tag_name('h2').get_attribute('textContent')
     elements = category.find_elements_by_tag_name('a')
     categories = [a.get_attribute('textContent') for a in elements]
     for i in categories: create_folder(i)
-    return categories
+    return elements, categories
 
-print(category_name())
+def get_sub_cateogories():
+    elements, categories = get_category_names()
+    i = 1
+    category_links = [e.get_attribute('href') for e in elements]
+    while i < len(categories):
+        for link in category_links:
+            driver.execute_script(f"window.open('{link}')")
+            driver.switch_to.window(driver.window_handles[i])
+            print(get_category_names())
+            time.sleep(5)
+            i += 1
+
+get_sub_cateogories()
+
